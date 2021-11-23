810378f275ce5e4013a66e107a9bd0a4117af4e9
Sue Holder <susanpholder@gmail.com>
2021-11-23 10:36:32 +0000

    cleaned up routes

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.get('/', function (req, res) {
     res.render('index');
 });
 
+// ----HOME
+
 app.post('/', function (req, res) {
     // get the form data
     const message = req.body;
@@ -32,6 +34,8 @@ app.post('/', function (req, res) {
     res.redirect('/confirm');
 });
 
+// ----ENQUIRY
+
 app.get('/enquiry', function (req, res) {
     res.render('enquiry');
 });
@@ -41,13 +45,15 @@ app.post('/enquiry', function (req, res) {
 
     const filePath = path.join(__dirname, 'data', 'enquiry.json');
     const fileData = fs.readFileSync(filePath);
-    const existingEnquiries = JSON.parse(fileData);
-    existingEnquiries.push(enquiry);
+    const storedEnquiries = JSON.parse(fileData);
+    storedEnquiries.push(enquiry);
 
-    fs.writeFileSync(filePath, JSON.stringify(existingEnquiries));
+    fs.writeFileSync(filePath, JSON.stringify(storedEnquiries));
 
     res.redirect('/confirm');
 });
+
+// ----CONTACT
 
 app.get('/contact', function (req, res) {
     res.render('contact');
@@ -74,6 +80,10 @@ app.get('/confirm', function (req, res) {
     res.render('confirm');
 });
 
+// ----DYNAMIC PAGES----
+
+// ----DASHBOARD
+
 app.get('/dashboard', function (req, res) {
     const messagesFilePath = path.join(__dirname, 'data', 'message.json');
     const messagesFileData = fs.readFileSync(messagesFilePath);
@@ -85,111 +95,15 @@ app.get('/dashboard', function (req, res) {
 
     res.render('dashboard', {
         numberOfMessages: storedMessages.length,
+        contacts: storedMessages,
         numberOfEnquiries: storedEnquiries.length,
+        enquiries: storedEnquiries,
     });
-});
-
-app.get('/about', function (req, res) {
-    res.render('about');
 });
 
 app.get('/our-work', function (req, res) {
     // route for dynamic content
     res.render('our-work');
 });
-
-app.get('/register', function (req, res) {
-    res.render('register');
-});
-
-app.get('/login', function (req, res) {
-    res.render('login');
-});
-
-app.get('/disclaimer', function (req, res) {
-    res.render('login');
-});
-
-app.get('/terms', function (req, res) {
-    res.render('terms-of-sale');
-});
-
-// app.post('/store-enquiry', function (req, res) {
-//     const enquiry = req.body;
-
-//     const filePath = path.join(__dirname, 'data', 'enquiry.json');
-//     const fileData = fs.readFileSync(filePath);
-//     const existingEnquiries = JSON.parse(fileData);
-//     existingEnquiries.push(enquiry);
-
-//     fs.writeFileSync(filePath, JSON.stringify(existingEnquiries));
-
-//     res.send('<h1>Request sent!</h1>');
-// });
-
-// app.get('/enquiries', function (req, res) {
-//     const filePath = path.join(__dirname, 'data', 'enquiry.json');
-
-//     const fileData = fs.readFileSync(filePath);
-//     const storedEnquiries = JSON.parse(fileData);
-
-//     let responseData = '<ol>';
-
-//     for (const enquiry of storedEnquiries) {
-//         responseData +=
-//             '<li><p>Date: ' +
-//             enquiry.posted +
-//             '</p><p>Name: ' +
-//             enquiry.name +
-//             '</p><p>Email: ' +
-//             enquiry.email +
-//             '</p><p>Message: ' +
-//             enquiry.message +
-//             '</p></li>';
-//     }
-
-//     responseData += '</ol>';
-
-//     res.send(responseData);
-// });
-
-// app.post('/store-message', function (req, res) {
-//     const message = req.body;
-
-//     const filePath = path.join(__dirname, 'data', 'message.json');
-//     const fileData = fs.readFileSync(filePath);
-//     const existingMessages = JSON.parse(fileData);
-//     existingMessages.push(message);
-
-//     fs.writeFileSync(filePath, JSON.stringify(existingMessages));
-
-//     res.send('<h1>Message saved!</h1>');
-// });
-
-// app.get('/messages', function (req, res) {
-//     const filePath = path.join(__dirname, 'data', 'message.json');
-
-//     const fileData = fs.readFileSync(filePath);
-//     const storedMessages = JSON.parse(fileData);
-
-//     let responseData = '<ol>';
-
-//     for (const message of storedMessages) {
-//         responseData +=
-//             '<li><p>Date: ' +
-//             message.posted +
-//             '</p><p>Name: ' +
-//             message.name +
-//             '</p><p>Email: ' +
-//             message.email +
-//             '</p><p>Message: ' +
-//             message.message +
-//             '</p></li>';
-//     }
-
-//     responseData += '</ol>';
-
-//     res.send(responseData);
-// });
 
 app.listen(3000);
