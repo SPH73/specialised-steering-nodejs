66356c460e116b94c40988ea86d0f81a15579992
Sue Holder <susanpholder@gmail.com>
2021-12-22 14:11:16 +0000

    added auth middleware

---
+++ middleware/auth-middleware.js
@@ -0,0 +1,24 @@
+const db = require('../data/db');
+
+const auth = async (req, res, next) => {
+    const user = req.session.user;
+    const isAuth = req.session.isAuthenticated;
+
+    if (!user || !isAuth) {
+        return next();
+    }
+
+    const userDoc = await db
+        .getDb()
+        .collection('staff')
+        .findOne({ _id: user.id });
+
+    const isAdmin = userDoc.isAdmin;
+
+    res.locals.isAdmin = isAdmin;
+    res.locals.isAuth = isAuth;
+
+    next();
+};
+
+module.exports = auth;
