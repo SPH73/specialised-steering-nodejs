4cbee65aafacfa7487a1f7b3b3e58408dee80dfe
Sue Holder <susanpholder@gmail.com>
2022-07-18 18:48:55 +0100

    added scripts

---
+++ public/js/upload-preview.js
@@ -0,0 +1,17 @@
+const filePickerEl = document.getElementById('image');
+const imagePreviewEl = document.getElementById('image-preview');
+
+function showPreview() {
+    const files = filePickerEl.files;
+    if (!files || files.length === 0) {
+        imagePreviewEl.style.display = 'none';
+        return;
+    }
+
+    const pickeFile = files[0];
+
+    imagePreviewEl.src = URL.createObjectURL(pickeFile);
+    imagePreviewEl.style.display = 'block';
+}
+
+filePickerEl.addEventListener('change', showPreview);
