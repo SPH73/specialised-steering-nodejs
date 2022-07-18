5d1ae43f5eddf76554dae3044889b7c7912cd721
Sue Holder <susanpholder@gmail.com>
2022-07-18 10:13:12 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const session = require('express-session');
-const compression = require('compression');
+const express = require("express");
+const session = require("express-session");
+const compression = require("compression");
 
-const csrf = require('csurf');
+const csrf = require("csurf");
 
-const sessionConfig = require('./utils/session');
-const db = require('./data/db');
+const sessionConfig = require("./utils/session");
+const db = require("./data/db");
 
-const path = require('path');
+const path = require("path");
 
-const authMiddleware = require('./middleware/auth-middleware');
-const addCSRFTokenMiddleware = require('./middleware/csrf-token-middlware');
-const errorsHandlerMiddleware = require('./middleware/error-handler');
+const authMiddleware = require("./middleware/auth-middleware");
+const addCSRFTokenMiddleware = require("./middleware/csrf-token-middlware");
+const errorsHandlerMiddleware = require("./middleware/error-handler");
 
-const dynamicRoutes = require('./routes/dynamic');
-const defaultRoutes = require('./routes/default');
-const authRoutes = require('./routes/auth');
+const dynamicRoutes = require("./routes/dynamic");
+const defaultRoutes = require("./routes/default");
+const authRoutes = require("./routes/auth");
 
 const mongodbSessionStore = sessionConfig.createSessionStore(session);
 
@@ -25,20 +25,20 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(compression());
 
 app.use(
-    express.static('public', {
-        etag: true,
-        maxAge: 31536000000,
-        redirect: true,
-    }),
+  express.static("public", {
+    etag: true,
+    maxAge: 31536000000,
+    redirect: true,
+  }),
 );
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: false, limit: '10mb' }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: false, limit: "10mb" }));
 
 // Middleware
 // Sessions config
@@ -60,22 +60,22 @@ app.use(authRoutes);
 // app.use(errorsHandlerMiddleware.handleNotFoundError);
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
+  res.status(404).render("404");
 });
 app.use((error, req, res, next) => {
-    res.status(500).render('500');
+  res.status(500).render("500");
 });
 
 // Connect to MongoDB when the server starts
 db.connectToDatabase(function (err) {
-    if (err) {
-        console.error(err);
-        process.exit();
-    }
+  if (err) {
+    console.error(err);
+    process.exit();
+  }
 
-    // start the Express server
-    app.listen(PORT, () => {
-        console.log(`Server is running on port: ${PORT}`);
-        console.log('http://localhost:3000');
-    });
+  // start the Express server
+  app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+    console.log("http://localhost:3000");
+  });
 });
